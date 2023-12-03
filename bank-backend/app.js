f54453028b4c8ea83fd11c5a44cb4335e95b9399
Jason Chan <jasonchan117@gmail.com>
2023-12-03 12:53:58 -0800

    mongostart

--- bank-backend/app.js
+++ bank-backend/app.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const mongoose = require("mongoose");
 const app = express()
-const mysql = require('mysql2/promise')
+//const mysql = require('mysql2/promise')
+
+//0 = mongo, 1 = sql
+useSQL = 1;
 
 // Start server
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+
+if(useSQL){
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+  })
+} else {
+  mongoose.connect(process.env.mongo_url)
+  .then(() =>{
+    // start the Express server
+    app.listen(PORT, () => {
+      console.log(`Server connected to mongo and is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
