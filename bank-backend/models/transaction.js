7951dcfe0f5c9b80d020f5bb8e1ef769e37e5c04
theboredkb <jiayangkb@gmail.com>
2023-12-04 11:12:30 -0800

    initial data

---
+++ bank-backend/models/transaction.js
@@ -0,0 +1,25 @@
+import { getTransactionsByUserId } from '../utils/_dbloader'
+
+function createFakeTransaction() {
+  return {
+    id: Math.random() * 1000,
+    userId: fakeTransactions.length + 1,
+    amount: Math.random * 1000000,
+    type: 'deposit',
+    createdAt: Date.now(),
+  }
+}
+
+//id: number
+export function getFakeTransactions(amount) {
+  return Array(amount).fill(createFakeTransaction())
+}
+
+//id: number
+export function getTransaction(id) {
+  try {
+    getTransactionsByUserId(id)
+  } catch (e) {
+    console.error(e)
+  }
+}
