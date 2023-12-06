aa192532956d0e7084d1137b904990d495ebeff9
Azi <subalt4amail@gmail.com>
2023-12-05 23:40:43 -0800

    Add react-router

---
+++ bank-frontend/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import { Card } from "react-bootstrap";
+
+const LoginPage = () => {
+    return ( 
+        <div className="content">
+            <Card>
+                <Card.Header>
+                    <Card.Title>Login</Card.Title>
+                </Card.Header>
+
+                <Card.Body>
+                    
+                </Card.Body>
+            </Card>
+        </div>
+     );
+}
+ 
+export default LoginPage;
